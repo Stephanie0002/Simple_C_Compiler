8d4763343d2065a06773f869f14c009e9ca657d3
yyt <2238275317@qq.com>
2021-05-10 11:47:52 +0800

    yyt: correct bash

--- code/run.bash
+++ code/run.bash
@@ -1,6 +1,4 @@
 flex cc.l
-@REM copy lex.yy.c lex.yy.cpp
 bison -vdty --locations cc.y
-@REM copy y.tab.c y.tab.cpp
 g++ -std=c++11 -o cc grammarTree.cpp utils.cpp lex.yy.c y.tab.c
 cc.exe true test1.c test2.c
